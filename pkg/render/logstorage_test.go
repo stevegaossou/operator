ed1fe7990a876df055ea1e98bcf42174703bd33e
vberezny <vlad@tigera.io>
2019-09-27 08:56:04 -0700

    WIP: added a check for expected pvc template, need to finish

--- pkg/render/logstorage_test.go
+++ pkg/render/logstorage_test.go
@@ -6,23 +6,39 @@ import (
 
 	operator "github.com/tigera/operator/pkg/apis/operator/v1"
 	"github.com/tigera/operator/pkg/render"
+	//corev1 "k8s.io/api/core/v1"
+	//"k8s.io/apimachinery/pkg/api/resource"
+	//metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-var _ = Describe("Logstorage rendering tests", func() {
+var _ = Describe("LogStorage rendering tests", func() {
 	var logStorage *operator.LogStorage
+	//storageClassName := "tigera-elasticsearch"
+	//expectedPvcTemplate := corev1.PersistentVolumeClaim{
+	//	ObjectMeta: metav1.ObjectMeta{
+	//		Name: "elasticsearch-data",
+	//	},
+	//	Spec: corev1.PersistentVolumeClaimSpec{
+	//		AccessModes: []corev1.PersistentVolumeAccessMode{corev1.ReadWriteOnce},
+	//		Resources: corev1.ResourceRequirements{
+	//			Requests: corev1.ResourceList{
+	//				"storage": resource.MustParse("10Gi"),
+	//			},
+	//		},
+	//		StorageClassName: &storageClassName,
+	//	},
+	//}
 	BeforeEach(func() {
 		// Initialize a default logStorage to use. Each test can override this to its
 		// desired configuration.
 		logStorage = &operator.LogStorage{
-			Spec: &operator.LogStorageSpec{
+			Spec: operator.LogStorageSpec{
 				Certificate: &operator.Certificate{
-					// TODO: determine correct secret name
 					SecretName: "tigera-es-config",
 				},
 				NodeConfig: &operator.NodeConfig{
 					NodeCount: 2,
 					StorageClass: "",
-					// TODO: determine which value to use
 					ResourceRequirements: nil,
 				},
 				IndexConfig: &operator.IndexConfig{
@@ -30,18 +46,20 @@ var _ = Describe("Logstorage rendering tests", func() {
 					ShardCount: 2,
 				},
 			},
-			// TODO: determine if needed
-			Status: &operator.LogStorageStatus{
+			// TODO: is this needed?
+			Status: operator.LogStorageStatus{
 			},
 		}
 
 	})
 
 	It("should render a LogStorageComponent", func() {
-		component := render.LogStorage(logStorage)
+		component, err := render.LogStorage(*logStorage)
 		resources := component.Objects()
 		Expect(len(resources)).To(Equal(2))
-		ExpectResource(resources[0], "tigera-elasticsearch", "", "", "v1", "StorageClass")
-		ExpectResource(resources[1], "tigera-elasticsearch", "tigera-elasticsearch", "", "v1", "Elasticsearch")
+		Expect(err).NotTo(HaveOccurred())
+		ExpectResource(resources[0], "tigera-elasticsearch", "", "", "", "")
+		ExpectResource(resources[1], "tigera-elasticsearch", "tigera-elasticsearch", "elasticsearch.k8s.elastic.co", "v1alpha1", "Elasticsearch")
+		//Expect(resources[1].Spec.Nodes[0].VolumeClaimTemplates).To(ConsistOf(expectedPvcTemplate))
 	})
 })
